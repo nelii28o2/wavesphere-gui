f2f1bd192d1d47d340ba4a769ec7f71e6306e351
Wilberto Lopez <sednus@gmail.com>
2013-03-28 21:50:10 -0400

    Add Left Panel to main window

--- WaveSphere/src/edu/uprm/icom5217/wave/view/LeftPanel.java
+++ WaveSphere/src/edu/uprm/icom5217/wave/view/LeftPanel.java
@@ -18,51 +18,65 @@ import javax.swing.JLabel;
 import java.awt.FlowLayout;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.UIManager;
 
 public class LeftPanel extends JPanel {
 	public LeftPanel() {
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setLayout(new GridLayout(0, 1, 0, 0));
 		
 		panel_1 = new JPanel();
 		panel_1.setBorder(new EmptyBorder(10, 10, 10, 10));
 		add(panel_1);
 		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.PAGE_AXIS));
-	
-		JList<String> spheresList = new JList<String>();
-		spheresList.setBackground(Color.WHITE);
-		spheresList.setPreferredSize(new Dimension(50, 200));
-		spheresList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		spheresList.setOpaque(false);
-		spheresList.setModel(new AbstractListModel<String>() {
+		
+			JList<String> spheresList = new JList<String>();
+			spheresList.setBackground(Color.WHITE);
+			spheresList.setPreferredSize(new Dimension(30, 100));
+			spheresList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			spheresList.setOpaque(false);
+			spheresList.setModel(new AbstractListModel() {
+				String[] values = new String[] {"Sphere1", "Sphere2", "Sphere3", "Sphere4"};
+				public int getSize() {
+					return values.length;
+				}
+				public Object getElementAt(int index) {
+					return values[index];
+				}
+			});
 			
-			private static final long serialVersionUID = 8041856790125168772L;
-			String[] values = new String[] {"Sphere1", "Sphere2"};
-			public int getSize() {
-				return values.length;
-			}
-			public String getElementAt(int index) {
-				return values[index];
-			}
-		});
-		spheresList.setMinimumSize(new Dimension(50, 400));
+			
+			scrollPane = new JScrollPane(spheresList);
+			scrollPane.setPreferredSize(new Dimension(50, 100));
+			scrollPane.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Local Spheres:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			panel_1.add(scrollPane);
+			scrollPane.setAlignmentX(Component.LEFT_ALIGNMENT);
+			scrollPane.setMinimumSize(new Dimension(50, 100));
 		
+		panel_2 = new JPanel();
+		add(panel_2);
+		panel_2.setLayout(new GridLayout(0, 1, 0, 0));
 		
-		scrollPane = new JScrollPane(spheresList);
-		scrollPane.setBorder(new TitledBorder(null, "Wave Spheres:", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_1.add(scrollPane);
-		scrollPane.setAlignmentX(Component.LEFT_ALIGNMENT);
-		scrollPane.setMinimumSize(new Dimension(40, 80));
-		scrollPane.setPreferredSize(new Dimension(50, 65));
+		panel = new JPanel();
+		panel_2.add(panel);
 		
 		
 		btnNewButton = new JButton("Add New Sphere");
-		add(btnNewButton);
+		panel.add(btnNewButton);
+		
+		panel_3 = new JPanel();
+		panel_2.add(panel_3);
 		
 		btnNewButton_1 = new JButton("Download All Data");
-		add(btnNewButton_1);
+		panel_3.add(btnNewButton_1);
+		
+		panel_4 = new JPanel();
+		panel_2.add(panel_4);
 		
 		btnNewButton_2 = new JButton("Delete All Data");
-		add(btnNewButton_2);
+		panel_4.add(btnNewButton_2);
 	}
 
 	/**
@@ -74,6 +88,10 @@ public class LeftPanel extends JPanel {
 	private JButton btnNewButton_2;
 	private JScrollPane scrollPane;
 	private JPanel panel_1;
+	private JPanel panel;
+	private JPanel panel_2;
+	private JPanel panel_3;
+	private JPanel panel_4;
   
 
 }
---
+++ WaveSphere/src/edu/uprm/icom5217/wave/view/MainWindow.java
@@ -0,0 +1,46 @@
+package edu.uprm.icom5217.wave.view;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+public class MainWindow extends JFrame{
+	
+	/**
+	 * The generated serial ID
+	 */
+	private static final long serialVersionUID = 6647353545887103818L;
+	
+	public MainWindow() {
+//		getContentPane().setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 0, 596, 379);
+		getContentPane().add(scrollPane);
+		
+		JSplitPane splitPane = new JSplitPane();
+		scrollPane.setViewportView(splitPane);
+		
+		JPanel leftPanel = new LeftPanel();
+		splitPane.setLeftComponent(leftPanel);
+		
+		JPanel panel_1 = new JPanel();
+		splitPane.setRightComponent(panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{0};
+		gbl_panel_1.rowHeights = new int[]{0};
+		gbl_panel_1.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+	}
+
+}
