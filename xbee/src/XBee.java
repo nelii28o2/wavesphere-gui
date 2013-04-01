5e774d6f20602a8c6c06f064c0ab11c9d950bd93
Nelian <Nelian@NeLii.choicecable.net>
2013-04-01 19:22:14 -0400

    added static variables to xbee lang

--- xbee/src/XBee.java
+++ xbee/src/XBee.java
@@ -4,6 +4,15 @@ public class XBee {
 	public class lang{
 		public static final int LED1 = 'A';
 		public static final int LED2 = 'B';
+		
+		public static final int ID = 0;
+		public static final int RETRIEVAL_MODE = 1;
+		public static final int SHUTDOWN_MODE = 2;
+		public static final int SAMPLING_MODE = 3;
+		public static final int STOP_LOCATE_MODE = 4;
+		public static final int DIAGNOSTIC_MODE = 5;
+		public static final int STOP_DIAGNOSTIC_MODE = 6;
+
 }
 
 }
--- xbee/src/serialMain.java
+++ xbee/src/serialMain.java
@@ -4,35 +4,32 @@ import gnu.io.UnsupportedCommOperationException;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintStream;
 import java.util.TooManyListenersException;
 
 
 public class serialMain {
 
 	public static void main(String[] args) throws IOException, PortInUseException, UnsupportedCommOperationException, TooManyListenersException{
-//		serialCommunication serial = new serialTest();
-//
-//		serial.openSerialPort("COM7", 9600);
-//
-//		InputStream inputStream = serial.getInputStream();
-//		PrintStream outputStream = serial.getOutputStream();
-//
-//		//do stuff
-//		byte[] b = new byte[2];
-//		outputStream.print("B");
-//		outputStream.print("\r\n");
-//		
-//		while(true);
-//		
-////		while(!serial.isDataAvailable());
-////		
-////		int num = inputStream.read(b);
-////		System.out.println(num);
-////		String s2 = new String(b,"US-ASCII");
-////		System.out.println(s2);
+		SerialCommunication serial = new SerialCommunication();
 
+		serial.openSerialPort("COM8", 9600);
 
-		//serial.close();
+		//InputStream inputStream = serial.getInputStream();
+		//PrintStream outputStream = serial.getOutputStream();
+
+		//do stuff
+		serial.write("hello world\0");
+		
+		while(true);
+		
+//		while(!serial.isDataAvailable());
+//		
+//		int num = inputStream.read(b);
+//		System.out.println(num);
+//		String s2 = new String(b,"US-ASCII");
+//		System.out.println(s2);
+//
+//
+//		serial.close();
 	}
 }
