508894a6da4ed76eb71d532aa8f6e287a47f7ed2
Wilberto Lopez <sednus@gmail.com>
2013-05-02 00:26:06 -0400

    no si hice todo.. verifica.. hay bugs..

--- WaveSphere/src/edu/uprm/icom5217/wave/view/diagnostic/DiagnosticPanel.java
+++ WaveSphere/src/edu/uprm/icom5217/wave/view/diagnostic/DiagnosticPanel.java
@@ -4,14 +4,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import net.miginfocom.swing.MigLayout;
 import edu.uprm.icom5217.wave.WaveSphere;
+import edu.uprm.icom5217.wave.view.MainWindow;
 import edu.uprm.icom5217.wave.xbee.Xbee;
 
-public class DiagnosticWindow extends JDialog {
+public class DiagnosticPanel extends JPanel {
 	
 	private static final long serialVersionUID = -5722644987144387448L;
 	private JLabel batteryLabel;
@@ -30,29 +31,25 @@ public class DiagnosticWindow extends JDialog {
 	private JLabel gyroValueLabel;
 	private JLabel magneticValueLabel;
 	
-	private static DiagnosticWindow INSTANCE;
+	private static DiagnosticPanel INSTANCE;
 	
-	public DiagnosticWindow() {
-		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		setResizable(false);
-		setTitle("Diagnosis Of Sphere 1");
-		getContentPane().setLayout(new MigLayout("", "[46.00][156.00,grow][118.00][46.00]", "[][][][][][][][][][]"));
-		getContentPane().add(getBatteryLabel(), "cell 1 0");
-		getContentPane().add(getBatteryValueLabel(), "cell 2 0");
-		getContentPane().add(getMemoryLabel(), "cell 1 1");
-		getContentPane().add(getMemoryValueLabel(), "cell 2 1");
-		getContentPane().add(getLocationLabel(), "cell 1 2");
-		getContentPane().add(getLocationValueLabel(), "cell 2 2");
-		getContentPane().add(getWirelessLabel(), "cell 1 3");
-		getContentPane().add(getWirelssValueLabel(), "cell 2 3");
-		getContentPane().add(getAccelerationLabel(), "cell 1 4");
-		getContentPane().add(getAccelerationValueLabel(), "cell 2 4");
-		getContentPane().add(getGyroLabel(), "cell 1 5");
-		getContentPane().add(getGyroValueLabel(), "cell 2 5");
-		getContentPane().add(getMagenticLabel(), "cell 1 6");
-		getContentPane().add(getMagneticValueLabel(), "cell 2 6");
-		getContentPane().add(getStopButton(), "cell 0 9 4 1,alignx center");
-		pack();
+	public DiagnosticPanel() {
+		setLayout(new MigLayout("", "[46.00][156.00,grow][118.00][46.00]", "[][][][][][][][][][]"));
+		add(getBatteryLabel(), "cell 1 0");
+		add(getBatteryValueLabel(), "cell 2 0");
+		add(getMemoryLabel(), "cell 1 1");
+		add(getMemoryValueLabel(), "cell 2 1");
+		add(getLocationLabel(), "cell 1 2");
+		add(getLocationValueLabel(), "cell 2 2");
+		add(getWirelessLabel(), "cell 1 3");
+		add(getWirelssValueLabel(), "cell 2 3");
+		add(getAccelerationLabel(), "cell 1 4");
+		add(getAccelerationValueLabel(), "cell 2 4");
+		add(getGyroLabel(), "cell 1 5");
+		add(getGyroValueLabel(), "cell 2 5");
+		add(getMagenticLabel(), "cell 1 6");
+		add(getMagneticValueLabel(), "cell 2 6");
+		add(getStopButton(), "cell 0 9 4 1,alignx center");
 	}
 	private JLabel getBatteryLabel() {
 		if (batteryLabel == null) {
@@ -163,8 +160,9 @@ public class DiagnosticWindow extends JDialog {
 				public void actionPerformed(ActionEvent e) {
 					WaveSphere.serial.write(Xbee.STOP_DIAGNOSTIC_MODE);
 					WaveSphere.serial.setFlag(Xbee.STATUS_MODE);
-					dispose();
-					
+					MainWindow.normalMode();
+					MainWindow.getInstance().pack();
+					MainWindow.getInstance().pack();
 				}
 				
 			});
@@ -172,9 +170,9 @@ public class DiagnosticWindow extends JDialog {
 		return stopButton;
 	}
 	
-	public static DiagnosticWindow getInstance(){
+	public static DiagnosticPanel getInstance(){
 		if(INSTANCE == null){
-			INSTANCE = new DiagnosticWindow();
+			INSTANCE = new DiagnosticPanel();
 		}
 		
 		return INSTANCE;
